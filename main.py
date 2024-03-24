2d53058324c2d0ee85ef10769e4f2ec28d06b710
EgorBud <tujhrf2003@gmail.com>
2024-03-24 11:23:18 +0300

    neibourhood

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
+np.set_printoptions(precision=3, suppress=True)
 
 n=4
 power=10
@@ -15,23 +15,24 @@ class sourse:
         self.x=x
         self.y=y
         self.power=power
-        self.inf=q
+        self.inf=Quantum(q)
     def shoot(self, aims):
         aim=max(aims, key=lambda aim:(aim.x-self.x)**2+(aim.y-self.y)**2)
-        aim.new(Quantum(self.inf), self.power)
+        aim.new(self.inf, self.power)
 class agent:
 
     def __init__(self):
-        self.neibours=[]
+        self.neibours={}
         self.quanta = []
         self.moral = np.array([0.0] * n)
         for i in range(n):
             self.moral[i]=random.random()
         self.x=random.random()
         self.y=random.random()
+        self.resist=random.random()
     def upd(self):
         for i in self.quanta:
-            self.moral+=np.array(i[0].inf)
+            self.moral+=np.array(i[0].inf)*self.resist
             i[1]-=1
         self.quanta=[i for i in self.quanta if i[1] != 0]
     def new(self, q, power):
@@ -39,25 +40,58 @@ class agent:
         power-=1
         if power!=0:
             for i in self.neibours:
-                i.new(q, power)
+                trust=1/2-np.linalg.norm(self.moral/np.linalg.norm(self.moral)-i.moral/np.linalg.norm(i.moral))/n
+                self.neibours[i]+=trust
+                if(self.neibours[i]<0):
+                    del self.neibours[i]
+                i.not_share(q, power, self, trust)
+
+    def share(self, q, power, sourse, trust):
+        self.neibours[sourse]+=trust
+        if (self.neibours[sourse] < 0):
+            del self.neibours[sourse]
+        self.quanta.append([q, power])
+        power-=1
+        if power!=0:
+
+            for i in self.neibours:
+                trust=1/2-np.linalg.norm(self.moral/np.linalg.norm(self.moral)-i.moral/np.linalg.norm(i.moral))/n
+                self.neibours[i]+=trust
+                if(self.neibours[i]<0):
+                    del self.neibours[i]
+                i.share(q, power, self, trust)
+    def not_share(self, q, power, sourse, trust):
+        self.neibours[sourse]+=trust
+        if (self.neibours[sourse] < 0):
+            del self.neibours[sourse]
+        self.quanta.append([q, power])
+        power-=1
+    def get_neib(self):
+        return [(i.x, i.y) for i in self.neibours.keys()]
+
     def show(self):
         print(n*self.moral/self.moral.sum())
-na=40
-neib=1
+na=50
+neib=4
 
 agents=[agent() for i in range(na)]
 for i in agents:
-    i.neibours=[agents[int(random.random()*na)] for j in range(neib)]
+    for j in range(neib):
+        bro=agents[int(random.random()*na)]
+        i.neibours[bro]=1
+        bro.neibours[i]=1
 ur=sourse([1, 0,0,0], 1, 1)
 ul=sourse([0, 1,0,0], 0, 1)
 lr=sourse([0, 0,1,0], 1, 0)
 ll=sourse([0, 0,0,1], 0, 0)
 sourses=[ur, ul, lr, ll]
-time=40
+time=50
+shoot_speed=3
 for t in range(time):
     print(t)
-    for i in sourses:
-        i.shoot(agents)
+    if (t%shoot_speed==0):
+        for i in sourses:
+            i.shoot(agents)
     for i in agents:
         i.upd()
 for i in agents:
@@ -66,3 +100,5 @@ for i in agents:
 plt.scatter([i.x for i in sourses], [i.y for i in sourses], c='r')
 plt.scatter([i.x for i in agents], [i.y for i in agents])
 plt.show()
+q=agents[int(random.random()*na)].get_neib()
+print(q)
