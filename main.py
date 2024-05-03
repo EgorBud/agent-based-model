a2b50658d918d69dc2a7e1cf8396fbee9e599e4c
EgorBud <tujhrf2003@gmail.com>
2024-05-03 23:14:30 +0300

    death

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ np.set_printoptions(precision=3, suppress=True)
 a=1/10
 n=4
 power=10
-cost=10
+share_cost=10
+birth_cost=100
+agents=[]
 class Quantum:
     def __init__(self, a):
         self.inf=a.copy()
@@ -23,8 +25,9 @@ class sourse:
             if dist<self.range:
                 aim.new(self.inf, int(self.power*(self.range-dist)))
 class agent:
+    def __init__(self, x=random.random(), y=random.random()):
 
-    def __init__(self):
+        self.dead=False
         self.neibours={}
         self.quanta = []
         self.moral = np.array([0.0] * n)
@@ -33,10 +36,11 @@ class agent:
         self.hist =[]
         for i in range(n):
             self.moral[i]=random.random()
-        self.x=random.random()
-        self.y=random.random()
+        self.x=x
+        self.y=y
         self.resist=random.random()
         self.age=1
+
     def upd(self):
         b=self.moral.sum()
         self.age+=1
@@ -53,37 +57,43 @@ class agent:
         self.energy+=self.useful
         self.hist.append(self.useful)
         #self.useful-=self.age*0.6
-        self.energy-=self.age*10
+        self.energy-=self.age*7.5
+        if self.energy >=birth_cost:
+            self.energy-=birth_cost
+            agents.append(agent(self.x+(random.random()-1/2)/10, self.y+(random.random()-1/2)/10))
         if self.energy<=0:
-            print('dead')
+            #print('dead')
+            self.dead=True
             del self
     def new(self, q, power):
         self.moral+=(np.array(q.inf)*power)
         power-=1
         if power > 0:
             for i in self.neibours:
-                if self.energy>cost:
+                if self.energy>share_cost:
                     i.share(q, power, self)
-                    self.energy-=cost
+                    self.energy-=share_cost
     def share(self, q, power, source):
         self.moral+=(np.array(q.inf)*power)
         power-=1
         if power > 0:
             for i in self.neibours:
                 if(i!=source):
-                    if self.energy>cost:
+                    if self.energy>share_cost:
                         i.new(q, power)
-                        self.energy-=cost
+                        self.energy-=share_cost
     def get_neib(self):
         return [(i.x, i.y) for i in self.neibours.keys()]
 
 
     def show(self):
-        print(self.moral, self.energy, self.useful)
+        print(self.moral, self.energy, self.useful, self.dead)
 na=20
 neib=4
 
-agents=[agent() for i in range(na)]
+
+for i in range(na):
+    agents.append(agent())
 for i in agents:
     for j in range(neib):
         bro=agents[int(random.random()*na)]
@@ -94,25 +104,26 @@ ul=sourse([0, 1,0,0], 0, 1)
 lr=sourse([0, 0,1,0], 1, 0)
 ll=sourse([0, 0,0,1], 0, 0)
 sourses=[ur, ul, lr, ll]
-time=50
-shoot_speed=1
+time=200
+shoot_pause=3
+
 for t in range(time):
     print(t)
-    if (t%shoot_speed==0):
+    if (t%shoot_pause==0):
         for i in sourses:
             i.shoot(agents)
     for i in agents:
         i.upd()
-
+    agents=list(filter(lambda x: not x.dead, agents))
 for i in agents:
     i.show()
-'''
+
 plt.scatter([i.x for i in sourses], [i.y for i in sourses], c='r')
 plt.scatter([i.x for i in agents], [i.y for i in agents])
 plt.show()
-'''
-q=agents[int(random.random()*na)].get_neib()
-plt.plot(np.exp(np.arange(1, time)/5))
-plt.plot(agents[int(random.random()*na)].hist)
+
+#q=agents[int(random.random()*len(agents))].get_neib()
+#plt.plot(np.exp(np.arange(1, time)/5))
+#plt.plot(agents[int(random.random()*na)].hist)
 plt.show()
-print(q)
+#print(q)
